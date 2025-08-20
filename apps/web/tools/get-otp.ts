56ffc0846e0e1129cc479dc12c6fc6df6ab1a692
Mario Cadenas <17888484+MarioCadenas@users.noreply.github.com>
2025-08-20 10:38:55 +0200

    fix: otp script (#379)

--- apps/web/tools/get-otp.ts
+++ apps/web/tools/get-otp.ts
@@ -1,5 +1,9 @@
 import * as OTPAuth from 'otpauth';
 
+if (!process.env.TOTP_SECRET) {
+  throw new Error('TOTP_SECRET is not set');
+}
+
 const totp = new OTPAuth.TOTP({ secret: process.env.TOTP_SECRET });
 const code = totp.generate();
 
